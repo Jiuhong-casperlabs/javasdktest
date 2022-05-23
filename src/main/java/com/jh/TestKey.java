fd5896d56b745e1741c840549230e15d10700b34
Jiuhong <jiuhong@casperlabs.io>
2022-05-23 10:41:54 +0800

    add testkey

---
+++ src/main/java/com/jh/TestKey.java
@@ -0,0 +1,43 @@
+package com.jh;
+
+import com.casper.sdk.service.signing.SigningService;
+import java.io.ByteArrayOutputStream;
+
+import java.io.IOException;
+import java.security.KeyPair;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+
+import com.casper.sdk.types.*;
+
+public class TestKey {
+    public static void main(String args[]) throws IOException {
+        final SigningService signingService = new SigningService();
+        final KeyPair keyPair = signingService.generateKeyPair(Algorithm.ED25519);
+
+        System.out.println("============");
+        System.out.println(keyPair.getPublic());
+        System.out.println("============");
+        // System.out.println(keyPair.getPrivate());
+
+        // create PRIVATE KEY file
+        final ByteArrayOutputStream privateOut = new ByteArrayOutputStream();
+        signingService.writeKey(privateOut, keyPair.getPrivate());
+        System.out.println(privateOut);
+        System.out.println("============");
+
+        // create PUBLIC KEY file
+        final ByteArrayOutputStream publicOut = new ByteArrayOutputStream();
+        signingService.writeKey(publicOut, keyPair.getPublic());
+        System.out.println(publicOut);
+        System.out.println("============");
+
+        // write private key to file
+        OutputStream spoutputStream = new FileOutputStream("private.pem");
+        privateOut.writeTo(spoutputStream);
+
+        // write public key to file
+        OutputStream pkoutputStream = new FileOutputStream("public.pem");
+        publicOut.writeTo(pkoutputStream);
+    }
+}
